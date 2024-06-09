e8fc8d3254ae3b0cba32b2ad373482a24bdea15b
eva046 <150832313+eva046@users.noreply.github.com>
2024-06-10 01:17:04 +0600

    Add files via upload

---
+++ Ascending_Sort.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,min,i,temp,j;
+    printf("Enter The Array Size - ");
+    scanf("%d",&n);
+    int A[n];
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&A[i]);
+    }
+    for(i=0; i<n; i++)
+    {
+        for(j=i+1; j<n; j++)
+        {
+            if(A[j]<A[i])
+            {
+                temp=A[j];
+                A[j]=A[i];
+                A[i]=temp;
+            }
+        }
+    }
+    for(i=0; i<n; i++)
+    {
+        printf("%d ",A[i]);
+    }
+}
+
